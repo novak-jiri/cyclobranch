6b0f2a880d0b809e2d80843dc6bc7b0a85d98bdf
Jiri Novak <jirinovak@atlas.cz>
2018-05-22 09:52:01 +0200

    imzml profile data support fixed (linux, os x)

---
+++ CycloBranch/External/linux/correctprofile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SAVEIFS=$IFS
+IFS=$',' read name fwhm <<< $@
+IFS=$SAVEIFS
+FileConverter -in "$name"".mgf" -out "$name"".FileConverter.mzML"
+NoiseFilterGaussian -in "$name"".FileConverter.mzML" -out "$name"".NoiseFilterGaussian.mzML" -algorithm:gaussian_width $fwhm
+rm "$name"".mzML"
+BaselineFilter -in "$name"".NoiseFilterGaussian.mzML" -out "$name"".mzML"
+rm "$name"".mgf"
+rm "$name"".FileConverter.mzML"
+rm "$name"".NoiseFilterGaussian.mzML"
